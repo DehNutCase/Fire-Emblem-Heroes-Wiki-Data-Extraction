74368e627751f834ff2f05c87275efe35879e7ab
DehNutCase <dslayerz@hotmail.com>
2017-08-07 18:07:55 -0700

    Update Hero_Info_Extractor.py

--- Hero_Info_Extractor.py
+++ Hero_Info_Extractor.py
@@ -13,7 +13,8 @@ for x in names:
         list_of_names.append(temp.group(1))
     
 #clearing 'None' entries from way I got the list of names
-temp = [x for x in list_of_names if (x != None) and (x != 'None')]
+temp = [x for x in list_of_names if (x != None) 
+    and (x != 'None') and (x != 'Special Maps')]
 list_of_names = temp
 """
 page.text(2) is weapons
@@ -55,6 +56,68 @@ for x in list_of_names:
     for y in temp:
         c_passive_list.append(y)
     output_dictionary[x]['C_Passives'] = c_passive_list
+    
+    #getting specials
+    
+    special_list = []
+    temp = re.findall(r"special\d=([\w ]+)", site.Pages[x].text(4))
+    for y in temp:
+        special_list.append(y)
+    output_dictionary[x]['Specials'] = special_list
+    
+    #getting color and weapon type (neutral = colorless)
+    
+    color = ''
+    weapon_type = ''
+    temp = re.findall(r"class ?= ?(\w+) (\w+)", site.Pages[x].text(0))
+    print(x)
+    print(temp)
+    if (temp):
+        color = temp[0][0]
+        weapon_type = temp[0][1]
+    output_dictionary[x]['Color'] = color
+    output_dictionary[x]['Weapon Type'] = weapon_type
+    
+    
+    #getting movement type
+    
+    temp = re.findall(r"Attribute ?= ?(\w+)", site.Pages[x].text(0))
+    movement_type = temp[0]
+    output_dictionary[x]['Movement'] = movement_type
+    
+    #getting 5* base stats, only going to output level 40 stats for now
+    
+    hp = []
+    atk = []
+    spd = []
+    defense = []
+    res = []
+    
+    temp = re.findall(r"Icon Rarity 5.+?span>(.+?)</table>", site.Pages[x].text(
+        0, expandtemplates=True))
+    stats = re.findall(r"<td>(.+?)</td>", str(temp))
+    level_1_stats = stats[0:5]
+    level_40_stats = stats[5:10]
+    
+    hp = level_40_stats[0]
+    atk = level_40_stats[1]
+    spd = level_40_stats[2]
+    defense = level_40_stats[3]
+    res = level_40_stats[4]
+    
+    level_40_stats_dictionary = { 'Hp' : hp, 'Attack' : atk, 'Speed' : spd,
+        'Defense' : defense, 'Resistance' : res}
+        
+    hp = level_1_stats[0]
+    atk = level_1_stats[1]
+    spd = level_1_stats[2]
+    defense = level_1_stats[3]
+    res = level_1_stats[4]   
+    
+    level_1_stats_dictionary = { 'Hp' : hp, 'Attack' : atk, 'Speed' : spd,
+        'Defense' : defense, 'Resistance' : res}
 
+    output_dictionary[x]['Level 40 Stats'] = level_40_stats_dictionary
+    output_dictionary[x]['Level 1 Stats'] = level_1_stats_dictionary
 
 print(json.dumps(output_dictionary, sort_keys=True, indent=4))
